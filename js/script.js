9bfa689447787f6762768e002e5dfc8eab6d955b
Brandon Carlos <brandon_marzo@hotmail.com>
2021-11-19 15:14:35 -0300

    Horario

---
+++ js/script.js
@@ -0,0 +1,27 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import { testando as teste, testandoFunction as funcaoPoderosa } from './modules/teste.js';
+import * as jogo from './modules/teste.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
+import initFuncionamento from './modules/funcionamento.js';
+
+console.log(jogo.senha);
+
+var nome = 'Brandon';
+
+teste();
+funcaoPoderosa();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
